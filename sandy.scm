1ce06fd0620997d153af590c5c1330888fe4f0fc
Lizzie Crowdagger <lizzie@crowdagger.fr>
2025-01-19 02:09:16 +0100

    Start playing with chickadee

--- sandy.scm
+++ sandy.scm
@@ -1,1 +1,57 @@
-(display "Pouet!\n")
+(install-r7rs!)
+
+(import (scheme base)
+        (chickadee)
+        (chickadee graphics path)
+        (chickadee graphics text)
+        (chickadee math vector)
+        (chickadee scripting))
+
+(define start-time 0.0)
+(define avg-frame-time 16.0)
+(define stats-text "Pouet")
+(define stats-position (vec2 4.0 100.0))
+(define position (vec2 160.0 240.0))
+(define text "The quick brown fox jumps over the lazy dog.\nFive hexing wizard bots jump quickly.")
+
+(define (stats-message)
+  (format #f "fps: ~1,2f, width: ~a"
+          (/ 1.0 avg-frame-time)
+          (window-width (current-window))))
+
+(define (load)
+  (script
+   (forever
+    (sleep 60)
+    (set! stats-text (stats-message)))))
+
+(define (draw alpha)
+  (let* ([width (window-width (current-window))]
+         [height (window-height (current-window))]
+         [position (vec2 (/ width 2.0)
+                         (/ height 2.0))])
+    (draw-text text position)
+    (draw-text stats-text stats-position)
+    (let ((current-time (elapsed-time)))
+      (set! avg-frame-time
+            (+ (* (- current-time start-time) 0.1)
+               (* avg-frame-time 0.9)))
+      (set! start-time current-time))))
+
+(define (update dt)
+  (update-agenda 1))
+
+(define (key-press key modifiers repeat?)
+  (when (eq? key 'q)
+    (abort-game)))
+
+
+(run-game #:draw draw
+          #:key-press key-press
+          #:load load
+          #:update update
+          #:window-title "text rendering"
+          #:window-width 1280
+          #:window-height 720)
+
+
