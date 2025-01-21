716157a94c1a35a5c1f57a0f06f1a6ed1a3ee6ff
Lizzie Crowdagger <lizzie@crowdagger.fr>
2025-01-21 02:55:23 +0100

    Add elements

---
+++ sandy/elements.scm
@@ -0,0 +1,17 @@
+(define-library (sandy elements)
+  (import (scheme base)
+          (crow-utils checked)
+          (rnrs enums))
+  (export element? element->u8 u8->element)
+  (begin
+    ;;; All  possible box elements
+    (define _elements '(empty sand))
+    (define elements (make-enumeration _elements))
+    (define element->u8 (enum-set-indexer elements))
+    (define-checked (element? (x symbol?))
+      (enum-set-member? x elements))
+    (define (u8->element n)
+      (if (>= n (length _elements))
+          (error "Invalid element")
+          (list-ref _elements n))) 
+    ))
